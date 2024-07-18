a45198c28cb54dbb6009758b54d5e6bf59f7cb97
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-07-17 23:46:30 -0500

    Se elimina el método

--- lista_reproduccion.py
+++ lista_reproduccion.py
@@ -37,13 +37,6 @@ class ListaReproduccion:
         self.ajustar_memoria()
         return True
     
-    #Ordenamiento y manipulación
-    def cambiar_orden(self, posicion_actual, nueva_posicion): #Se crea el método para cambiar el órden
-        if 0 <= posicion_actual < len(self.canciones) and 0 <= nueva_posicion < len(self.canciones):
-            cancion = self.canciones[posicion_actual]
-            del self.canciones[posicion_actual]
-            self.canciones.insert(nueva_posicion, cancion)
-    
     def ajustar_memoria(self):
         if self.tamaño < 0:
             self.tamaño = 0
