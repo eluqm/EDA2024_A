79e0e29614a776d02a54d657f644c15c5aec8329
Rommel0569 <rchambive@unsa.edu.pe>
2024-07-17 23:07:53 -0500

    Creando la clase lista_reproduccion, importando las clases nodo y cancion para las funcion de agregar, eliminar y mostrar lista de cancion

---
+++ lista_reproduccion.py
@@ -0,0 +1,15 @@
+from cancion import Cancion
+from nodo import Nodo
+
+class ListaReproduccion:
+    def __init__(self):
+        
+    def agregar_cancion(self, cancion):
+        
+    def eliminar_cancion(self, titulo):
+       
+    def ajustar_memoria(self):
+        
+    def __len__(self):   
+        
+    def mostrar_lista(self):
