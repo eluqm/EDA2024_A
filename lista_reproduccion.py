1c7b3dc018c924a3549cb9dc5cf4cb7b44bf7834
Rommel0569 <rchambive@unsa.edu.pe>
2024-07-17 23:09:30 -0500

    Agregando logica para la funcion de agregar cancion al linkedlist, para la lista de reproduccion

--- lista_reproduccion.py
+++ lista_reproduccion.py
@@ -8,6 +8,16 @@ class ListaReproduccion:
         self.tamaño = 0
         
     def agregar_cancion(self, cancion):
+        nuevo_nodo = Nodo(cancion)
+        if not self.cabeza:
+            self.cabeza = nuevo_nodo
+            self.cola = nuevo_nodo
+        else:
+            self.cola.siguiente = nuevo_nodo
+            nuevo_nodo.anterior = self.cola
+            self.cola = nuevo_nodo
+        self.tamaño += 1
+        self.ajustar_memoria()
         
     def eliminar_cancion(self, titulo):
        
