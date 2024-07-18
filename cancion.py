0db5fd77a25e0cb11ce014adaf2a1a67dcb6be6f
Rommel0569 <rchambive@unsa.edu.pe>
2024-07-17 23:00:27 -0500

    Creando la clase cancion para la lista de reproducción y agregando posibles datos

---
+++ cancion.py
@@ -0,0 +1,8 @@
+class Cancion:
+    def __init__(self, titulo, artista, album, año, duracion, popularidad):
+        self.titulo = titulo
+        self.artista = artista
+        self.album = album
+        self.año = año
+        self.duracion = duracion
+        self.popularidad = popularidad
