439f82f48dcf61ff1fb0289c9b92fd723f3ee359
Rommel0569 <rchambive@unsa.edu.pe>
2024-07-17 23:10:31 -0500

    Modificando la funcion de eliminar cancion, agregando logica para buscar la cancion y eliminar del linkedlist

--- lista_reproduccion.py
+++ lista_reproduccion.py
@@ -20,7 +20,23 @@ class ListaReproduccion:
         self.ajustar_memoria()
         
     def eliminar_cancion(self, titulo):
-       
+        actual = self.cabeza
+        while actual and actual.cancion.titulo != titulo:
+            actual = actual.siguiente
+        if not actual:
+            return False  # La canción no se encontró
+        if actual.anterior:
+            actual.anterior.siguiente = actual.siguiente
+        if actual.siguiente:
+            actual.siguiente.anterior = actual.anterior
+        if actual == self.cabeza:
+            self.cabeza = actual.siguiente
+        if actual == self.cola:
+            self.cola = actual.anterior
+        self.tamaño -= 1
+        self.ajustar_memoria()
+        return True
+    
     def ajustar_memoria(self):
         
     def __len__(self):   
