38062cc89499e3704890d08eb8b9e9f16108a359
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2024-07-25 23:16:21 -0500

    se cambia de archivo el nodo del arbol rojo-negro

--- nodos.py
+++ nodos.py
@@ -3,3 +3,12 @@ class Nodo:
         self.cancion = cancion
         self.siguiente = None
         self.anterior = None
+
+#Nodo para arbol BST
+class NodoRN:
+    def __init__(self, cancion, color="rojo"):
+        self.cancion = cancion
+        self.color = color  # "rojo" o "negro"
+        self.izquierda = None
+        self.derecha = None
+        self.padre = None
